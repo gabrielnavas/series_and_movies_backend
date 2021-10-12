e39bccdce3d5b0d629a1e5be69bbe178bda60d70
Gabriel Navas <gabnavas.dev@gmail.com>
2021-10-12 15:01:59 -0300

    add tables

--- modules/streams/controllers.py
+++ modules/streams/controllers.py
@@ -1,10 +1,23 @@
 from typing import Optional
-from fastapi import APIRouter, status, Response
+from fastapi import APIRouter, status, Response, status
 from pydantic import BaseModel
 
 from modules.streams.models import Stream
 
 router = APIRouter()
+
+
+@router.post("/api/streams")
+async def create_stream(stream_body: StreamBody, response: Response):
+    try:
+        stream = Stream.create(
+            name=stream_body.name
+        )
+        return {"my stream": stream.__data__}
+    except Exception as ex:
+        print(ex)
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {"detail": 'server error'}
 
 
 @router.get("/api/streams")
@@ -21,11 +34,3 @@ async def get_streams(page: Optional[int] = 0, count_of_page: Optional[int] = 10
 
 class StreamBody(BaseModel):
     name: str
-
-
-@router.post("/api/streams")
-async def create_stream(stream_body: StreamBody):
-    stream = Stream.create(
-        name=stream_body.name
-    )
-    return {"my stream": stream.__data__}
