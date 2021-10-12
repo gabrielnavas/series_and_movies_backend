fbd066351a03fc504c8b6052281f798dd3d06e19
Gabriel Navas <gabnavas.dev@gmail.com>
2021-10-12 12:57:23 -0300

    add tables

--- modules/shared/models.py
+++ modules/shared/models.py
@@ -6,11 +6,7 @@ from playhouse.db_url import connect
 
 DATABASE_URL = os.environ['DATABASE_URL']
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-
 # database_name = 'series_and_movies_db'
-
 # pg_db = peewee.PostgresqlDatabase(database_name, user='postgres', password='1234',
 #                                   host='127.0.0.1', port=5435)
 
